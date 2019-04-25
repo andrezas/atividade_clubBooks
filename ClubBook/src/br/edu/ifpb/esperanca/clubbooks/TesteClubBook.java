ff0a42718b859e5ec9acd41e758f639ad34efee1
Andreza <andreza.santana@academico.ifpb.edu.br>
2019-04-25 15:46:03 -0300

    Teste falho - 25/04

--- ClubBook/src/br/edu/ifpb/esperanca/clubbooks/TesteClubBook.java
+++ ClubBook/src/br/edu/ifpb/esperanca/clubbooks/TesteClubBook.java
@@ -6,7 +6,19 @@ class TesteClubBook {
 
 	@Test
 	void criarGrupo() {
+		MeuProjetoTeste m; 
+		ArrayList<Usuario> usuarios = new ArrayList<Usuario>
+		Grupo g = m.criarGrupo("Teste e DAW2", usuarios);
+		assertNotNull(g);
+		assertEquals("Teste e DAW2", g.getNomeGrupo());
+	}
 	
-
+	@Test
+	void cadastrarLiv() {
+		MeuProjetoTeste b; 
+		Livro livro = new Livro
+		Livro l = b.cadastrarLiv("SCRUM", livro);
+		assertNotNull(l);
+		assertEquals("SCRUM", l.getTitulo());
 	}
 }
